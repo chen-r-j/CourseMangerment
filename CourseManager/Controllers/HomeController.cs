a7e0a0e74812be14af409da79a8492fb0516d0e6
chen-rj <2217503884@qq.com>
2019-12-16 10:45:19 +0800

    创建项目

---
+++ CourseManager/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using CourseManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CourseManagerEntities db = new CourseManagerEntities();
+
+        public ActionResult Index()
+        {
+            var sidebars = db.SideBars.ToList();
+            ViewBag.SideBars = sidebars;
+            // var siteInfo = new WebsiteInfo("Demo", "RIGHT");
+            //ViewBag.SiteInfo = siteInfo;
+
+            // ViewData["SiteInfo"] = siteInfo;
+
+            // if(new Random().Next(2)==0)
+
+            // return View("~/Views/Home/View.cshtml");
+            // else
+           
+             return View();
+
+           // return View("~/Views/Home/View2.cshtml");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [ChildActionOnly]
+
+        public ActionResult NavBar()
+        {
+            var site = new WebsiteInfo();
+            ViewBag.Site = site;
+            
+            return PartialView("~/Views/Shared/NavBar.cshtml");
+        }
+        public ActionResult SideBar()
+        {
+            var sidebars = db.SideBars.ToList();
+            ViewBag.SideBars = sidebars;
+
+            return PartialView("~/Views/Shared/SideBar.cshtml");
+        }
+
+        
+    }
+}
---
+++ CourseManager/Models/ActionLink.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseManager.Models
+{
+    public class ActionLink
+    {
+        public string Name { get; set; }
+
+        public string Controller { get; set; }
+
+
+        public string Action { get; set; }
+    }
+}
